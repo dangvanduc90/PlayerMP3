7793ba007e1ee594321b388218e363ab83ed0364
dangvanduc90 <kid_1412aA>
2019-07-31 22:30:33 +0700

    update txt

--- app/src/main/java/com/example/duyphuong/playermp3/Activity/AboutActivity.java
+++ app/src/main/java/com/example/duyphuong/playermp3/Activity/AboutActivity.java
@@ -30,14 +30,14 @@ public class AboutActivity extends AppCompatActivity {
         lvIcon = (ListView) findViewById(R.id.lv_about_us);
         dsContents = new ArrayList<>();
 
-        dsContents.add(new Contents("Player Mp3","Made by NDP",R.drawable.icon));
-        dsContents.add(new Contents("Lập trình di động nâng cao","Mục tiêu: Miễn thi :))",android.R.drawable.star_on));
+        dsContents.add(new Contents("Player Mp3","Made by BHT",R.drawable.icon));
+        dsContents.add(new Contents("Lập trình di động nâng cao","",android.R.drawable.star_on));
         dsContents.add(new Contents("Build Version","Version 1.0",android.R.drawable.ic_dialog_info));
-        dsContents.add(new Contents("Email","nguyenduyphuong.hou@gmail.com",android.R.drawable.ic_dialog_email));
+        dsContents.add(new Contents("Email","buihuuthang97tb@gmail.com",android.R.drawable.ic_dialog_email));
         dsContents.add(new Contents("Share","Share to your friends",android.R.drawable.ic_menu_share));
         dsContents.add(new Contents("Rate","Give your rate and feedback",android.R.drawable.ic_menu_preferences));
         dsContents.add(new Contents("More","More apps from developer",android.R.drawable.ic_menu_more));
-        dsContents.add(new Contents("Copyright © 2018 Hou","All rights reserved",android.R.drawable.ic_secure));
+        dsContents.add(new Contents("Copyright © 7-2019","All rights reserved",android.R.drawable.ic_secure));
 
         contentsAdapter = new ContentsAdapter(AboutActivity.this,R.layout.items,dsContents);
         lvIcon.setAdapter(contentsAdapter);
