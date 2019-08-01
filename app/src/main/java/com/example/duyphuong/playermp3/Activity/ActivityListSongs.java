d126095e2b4985ee73c9e744b065a8a1d60626c8
dangvanduc90 <kid_1412aA>
2019-08-01 08:59:34 +0700

    update

--- app/src/main/java/com/example/duyphuong/playermp3/Activity/ActivityListSongs.java
+++ app/src/main/java/com/example/duyphuong/playermp3/Activity/ActivityListSongs.java
@@ -5,21 +5,32 @@ import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.support.v7.widget.Toolbar;
 
 import com.example.duyphuong.playermp3.Adapter.ListSongAdapter;
+import com.example.duyphuong.playermp3.Model.SongModel;
 import com.example.duyphuong.playermp3.R;
 
+import java.util.ArrayList;
 import java.util.function.ToLongBiFunction;
 
 public class ActivityListSongs extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private Toolbar toolbar;
     private ListView lvListSong;
+    private EditText inputSearch;
+    private ArrayList data;
+    private ArrayList dataNew;
+    private ListSongAdapter listSongAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,18 +47,59 @@ public class ActivityListSongs extends AppCompatActivity implements AdapterView.
     }
 
     private void loadEvents() {
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+            }
 
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+                String keyword = editable.toString();
+//                String keyword = edtKeyword.getText().toString().trim();
+                int text_length = keyword.length();
+                Log.d("111", keyword);
+                Log.d("111", data.size() + "");
+
+                dataNew.clear();
+                if (text_length > 0) {
+                    for (int i = 0; i < data.size(); i++) {
+                        Log.d("111", "data");
+
+                        SongModel mCongViec = (SongModel) data.get(i);
+
+                        if (mCongViec.getTitle().toLowerCase().contains(keyword.toLowerCase())) {
+                            dataNew.add(mCongViec);
+                        }
+                    }
+                } else {
+                    dataNew.addAll(data);
+                }
+                listSongAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     private void loadControls() {
         lvListSong = findViewById(R.id.lvListsong);
+        inputSearch = (EditText) findViewById(R.id.inputSearch);
 
         toolbar = findViewById(R.id.toolbarListSongs);
             toolbar.setTitle("Danh sach nhac");
             toolbar.setNavigationIcon(R.drawable.back);
             setSupportActionBar(toolbar);
 
-        ListSongAdapter listSongAdapter = new ListSongAdapter(this,MainActivity.arrSong);
+        data =  MainActivity.arrSong;
+        dataNew = data;
+
+        listSongAdapter = new ListSongAdapter(this, dataNew);
         lvListSong.setAdapter(listSongAdapter);
         lvListSong.setOnItemClickListener(this);
 
--- app/src/main/java/com/example/duyphuong/playermp3/Model/SongModel.java
+++ app/src/main/java/com/example/duyphuong/playermp3/Model/SongModel.java
@@ -3,4 +3,12 @@ package com.example.duyphuong.playermp3.Model;
 public class SongModel {
     public String title;
     public String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
